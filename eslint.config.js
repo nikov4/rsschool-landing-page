e3b9f7cbaf49a59b6428a27f4edc7a7bbea5bd2a
Nikolai O. <nik@o84.ru>
2026-09-17 14:36:58 +0300

    chore: add eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import js from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+export default [
+  js.configs.recommended,
+  eslintPluginPrettierRecommended,
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+    },
+  },
+];
